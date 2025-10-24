a7888e085e3a249736ecaac86827d6ac3c137ba7
Min-byeonghyun <qudgus5125@naver.com>
2025-10-24 20:14:13 +0900

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <div>
-      hello
+      <div className="text-xs text-red-500">text-xs</div>
+      <div className="text-sm">text-xm</div>
+      <div className="text-lg">text-lg</div>
+      <div className="text-xl">text-xl</div>
+      <div className="text-2xl">text-2xl</div>
+      <div className="text-[13px]">text-2xl</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
