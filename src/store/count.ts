359ac293afd18a128dba0513b89b9bf3cb33dacd
Min-byeonghyun <qudgus5125@naver.com>
2025-10-30 18:11:31 +0900

    middleware subscribe, persist, createJSONStorage, devtools

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,5 +1,11 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 // type Store = {
@@ -11,22 +17,48 @@ import { immer } from "zustand/middleware/immer";
 // };
 
 export const useCountStore = create(
-  immer(
-    combine({ count: 0 }, (set, get) => ({
-      actions: {
-        increase: () => {
-          set((state) => {
-            state.count += 1;
-          });
-        },
-        decrease: () => {
-          set((state) => {
-            state.count -= 1;
-          });
-        },
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase: () => {
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decrease: () => {
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
+            },
+          })),
+        ),
+      ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          count: store.count,
+        }),
+        storage: createJSONStorage(() => sessionStorage),
       },
-    })),
+    ),
+    {
+      name: "countStore",
+    },
   ),
+);
+
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // 2번째 콜백함수 =  Listner 함수
+    console.log(count, prevCount);
+    const store = useCountStore.getState();
+    // useCountStore.setState((store) => ({ }));
+  },
 );
 
 // export const useCountStore = create<Store>((set, get) => ({
