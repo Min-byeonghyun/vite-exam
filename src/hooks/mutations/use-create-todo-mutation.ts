9a783c934d466268b1aeb973dbac1016669762b1
Min-byeonghyun <qudgus5125@naver.com>
2025-11-02 22:07:20 +0900

    데이터 무효화

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,13 +1,16 @@
 import { createTodo } from "@/api/create-todo";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
+  const queryClient = useQueryClient(); // 쿼리클라이언트 store 모든 데이터를 저장하는 저장소
+
   return useMutation({
     mutationFn: createTodo,
     onMutate: () => {}, //요청 발송
     onSettled: () => {}, // 요청 종료
     onSuccess: () => {
-      window.location.reload();
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.todo.list }); //데이터 리패칭
     }, // 요청 성공
     onError: (error) => {
       window.alert(error.message);
